5c0e31d82bce8b26a38dc1a60a057d676ac4971b
David Franca <dvdgmf@gmail.com>
2022-08-16 15:32:58 +0200

    refactoring

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ import ftplib
 import logging
 import datetime
 import pymongo
-import decouple
 import pandas as pd
-
-from pathlib import Path
-# from decouple import config, Config, RepositoryEnv
+from decouple import config
 
 
 class Spectroman:
@@ -17,32 +14,19 @@ class Spectroman:
 
     def __init__(self, input_arguments=None):
 
-        # Test if a settings.ini file path was given
-        if len(input_arguments) > 1:
-            settings_dot_ini = Path(input_arguments[1])
-        else:
-            settings_dot_ini = None
-
-        self.config_data = self.get_config_dict(ini_path=settings_dot_ini)
+        self.config_data = self.get_config_dict()
         self.INSTANCE_TIME_TAG = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
         logfile = os.path.join(self.config_data['OUTPUT'], 'spectroman_' + self.INSTANCE_TIME_TAG + '.log')
         self.log = self.create_log_handler(logfile)
         self.log.info(f'Running Spectral Manager from: {sys.path[0]}')
         self.log.info(f'Saving LOG at: {logfile}')
-        self.log.info(f'Input arguments: {settings_dot_ini}')
+        self.log.info(f'Input arguments: {input_arguments}')
 
     @staticmethod
-    def get_config_dict(ini_path=None):
+    def get_config_dict():
         """
-        Access the settings.ini file in a given ini_path and return it as a dict.
-        If no ini_path is given, the default is used in the root
+        Access the settings.ini file and return it as a dict.
         """
-        if ini_path:
-            envconf = decouple.Config(decouple.RepositoryEnv(source=ini_path))
-            config = envconf.get
-        else:
-            config = decouple.config
-
         setup_dict = {
             'HOST': config('HOST_ADDRESS'),
             'USER': config('USER'),
@@ -51,8 +35,8 @@ class Spectroman:
             'FTP_TODO': config('FTP_TODO'),
             'FTP_DONE': config('FTP_DONE'),
             'FTP_FAIL': config('FTP_FAIL'),
-            'OUTPUT': config('OUTPUT_PATH'),
-            'DBCON': config('CONN_STR'),
+            'OUTPUT': config('OUTPUT'),
+            'DBCON': config('DBCON'),
             'DB_NAME': config('DB_NAME'),
             'COLLECTION_NAME': config('COLLECTION_NAME')
         }
@@ -91,7 +75,7 @@ class Spectroman:
         client = pymongo.MongoClient(connection_string)
         db = client[db_name]
         collection = db[collection_name]
-        return collection
+        return collection, client
 
     def get_station_data_df(self, csv_file):
         # get only the column names from the file
@@ -146,63 +130,69 @@ def main(in_args=None):
     manager.log.info('Attempting to copy...')
     # Destination = manager.config_data['OUTPUT']
 
-    spectral_collection = manager.get_mongo_connection()
-
-    for n, fn in enumerate(filenamepath):
-        file_fail_flag = False
-        # localcopy = Path(Destination + os.path.basename(fn))
-        manager.log.info(f'Processing {n + 1} of {total}: {fn}...')
-        # manager.log.info(f'Saving copy at: {localcopy}')
-        # ftp.retrbinary('RETR ' + fn, open(localcopy, 'wb').write)
-        manager.log.info(f'Creating empty dummy file locally.')
-        # https://stackoverflow.com/questions/48815110/read-a-csv-file-stored-in-a-ftp-in-python
-        virtual_file = io.BytesIO()
-        manager.log.info(f'Downloading remote file content to local dummy copy...')
-        ftp.retrbinary("RETR {}".format(fn), virtual_file.write)
-        # after writing: go back to the start of the virtual file
-        manager.log.info(f'Download completed.')
-        # after writing: go back to the start of the virtual file
-        virtual_file.seek(0)
-
-        # Test for empty files
-        fsize = virtual_file.getbuffer().nbytes
-        if fsize < 1000:
-            file_fail_flag = True
-        else:
-            manager.log.info(f'Attempting to parse virtual file into a pd.DataFrame.')
+    spectral_collection, client = manager.get_mongo_connection()
+    try:
+        for n, fn in enumerate(filenamepath):
+            file_fail_flag = False
+            # localcopy = Path(Destination + os.path.basename(fn))
+            manager.log.info(f'Processing {n + 1} of {total}: {fn}...')
+            # manager.log.info(f'Saving copy at: {localcopy}')
+            # ftp.retrbinary('RETR ' + fn, open(localcopy, 'wb').write)
+            manager.log.info(f'Creating empty dummy file locally.')
+            # https://stackoverflow.com/questions/48815110/read-a-csv-file-stored-in-a-ftp-in-python
+            virtual_file = io.BytesIO()
+            manager.log.info(f'Downloading remote file content to local dummy copy...')
+            ftp.retrbinary("RETR {}".format(fn), virtual_file.write)
+            # after writing: go back to the start of the virtual file
+            manager.log.info(f'Download completed.')
+            # after writing: go back to the start of the virtual file
             virtual_file.seek(0)
-            df = manager.get_station_data_df(virtual_file)
-            manager.log.info(f'Rows found in file:{len(df)}')
 
-        # If the file passed the size test, check if the output df has something inside.
-        if not file_fail_flag:
-            if df.empty or len(df) < 1:
-                manager.log.info(f'ERROR: insufficient rows in DataFrame, skipping insertion in mongoDB.')
+            # Test for empty files
+            fsize = virtual_file.getbuffer().nbytes
+            if fsize < 1000:
                 file_fail_flag = True
             else:
-                # turn every df row into an dictionary entry and store them in a list
-                df_rows_list = df.to_dict('records')
-                # insert the list of rows in mongoDB
-                dbname = manager.config_data['DB_NAME']
-                collectionname = manager.config_data['COLLECTION_NAME']
-                manager.log.info(f'Inserting data into {dbname}:{collectionname}.')
-                results = spectral_collection.insert_many(df_rows_list)
-                manager.log.info(f'Success.')
-                manager.log.info(results.inserted_ids)
+                manager.log.info(f'Attempting to parse virtual file into a pd.DataFrame.')
+                virtual_file.seek(0)
+                df = manager.get_station_data_df(virtual_file)
+                manager.log.info(f'Rows found in file:{len(df)}')
 
-        manager.log.info(f'Parsing completed, closing virtual file.')
-        virtual_file.close()
+            # If the file passed the size test, check if the output df has something inside.
+            if not file_fail_flag:
+                if df.empty or len(df) < 1:
+                    manager.log.info(f'ERROR: insufficient rows in DataFrame, skipping insertion in mongoDB.')
+                    file_fail_flag = True
+                else:
+                    # turn every df row into an dictionary entry and store them in a list
+                    df_rows_list = df.to_dict('records')
+                    # insert the list of rows in mongoDB
+                    dbname = manager.config_data['DB_NAME']
+                    collectionname = manager.config_data['COLLECTION_NAME']
+                    manager.log.info(f'Inserting data into {dbname}:{collectionname}.')
+                    results = spectral_collection.insert_many(df_rows_list)
+                    manager.log.info(f'Success.')
+                    manager.log.info(results.inserted_ids)
 
-        if file_fail_flag:
-            target = manager.config_data['FTP_FAIL'] + os.path.basename(fn)
-        else:
-            target = manager.config_data['FTP_DONE'] + os.path.basename(fn)
+            manager.log.info(f'Parsing completed, closing virtual file.')
+            virtual_file.close()
 
-        manager.log.info(f'Moving file {fn} to {target}.')
-        ftp.rename(fn, target)
-        manager.log.info(f'Done.')
+            if file_fail_flag:
+                target = manager.config_data['FTP_FAIL'] + os.path.basename(fn)
+            else:
+                target = manager.config_data['FTP_DONE'] + os.path.basename(fn)
 
-    ftp.quit()
+            manager.log.info(f'Moving file {fn} to {target}.')
+            ftp.rename(fn, target)
+            manager.log.info(f'Done.')
+
+    except Exception as e:
+        manager.log.info(e)
+        client.close()
+
+    finally:
+        client.close()
+        ftp.quit()
 
 
 if __name__ == "__main__":
