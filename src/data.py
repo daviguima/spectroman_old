c5297d2143f04e6c13d89e4fd8270d081fbc5669
lambdart <lambdart@protonmail.com>
2023-12-03 23:46:33 -0300

    Use na_values on read_csv function

--- src/data.py
+++ src/data.py
@@ -1,27 +1,46 @@
 import numpy as np
 import pandas as pd
 
-def csv_to_df(file):
-    with open(file, encoding="utf-8") as f:
+def csv_to_df(csv):
+    """
+    Read csv file and parse it to pandas data frame.
+    """
+    with open(csv, encoding="utf-8") as f:
         df = pd.read_csv(f,
                          sep=',',
                          skiprows=1,
-                         on_bad_lines='skip',
+                         on_bad_lines='error',
+                         na_values=[-99, '',
+                                    '-INF', 'INF',
+                                    '-inf', 'inf'
+                                    'Smp', 'TS'],
                          keep_default_na=True)
     return df
 
 def clean_df(df):
-    df.replace([np.inf, -np.inf], np.nan, inplace=True)
-    df.dropna(inplace=True)
+    """
+    Remove NA values from the data frame (df).
+    """
+    df.dropna(axis=0, how='any', inplace=True)
     return df
 
 def convert_dtypes(df):
+    """
+    Convert objects to numeric values.
+    """
     df = df.apply(pd.to_numeric, errors='ignore')
     return clean_df(df)
 
 def convert_datetime(df):
+    """
+    Convert time stamp string to date time.
+    """
     df['TIMESTAMP'] = pd.to_datetime(df['TIMESTAMP'], errors='ignore')
     return df
 
 def process_df(df):
+    """
+    Process data frame: clean, convert objects to numeric values and
+    time stamp to datetime.
+    """
     return convert_datetime(convert_dtypes(clean_df(df)))
