a9fa1871fd2d88947722d9c8d8dbb274f3e45b58
lambdart <lambdart@protonmail.com>
2023-12-23 22:43:58 -0300

    Add util functions

--- src/util.py
+++ src/util.py
@@ -1,6 +1,13 @@
+# builtin library
 from os import listdir
-from os.path import isfile, join, abspath
+from os.path import isfile, join, abspath, basename
+from io import StringIO
 
+# 3rd party libraries
+import numpy as np
+from scipy.interpolate import griddata
+
+# internal modules
 from conf import conf
 
 def list_csvs(path=None):
@@ -15,3 +22,28 @@ def list_csvs(path=None):
         if isfile(csv):
             csvs.append(csv)
     return csvs
+
+def open_file(fname):
+    return open(fname)
+
+def file_to_str_io(fname):
+    with open(fname) as f:
+        return StringIO(f.read())
+
+def list_date():
+    date = []
+    for f in list_csvs():
+        date.append(basename(f).split('_')[0])
+    return date
+
+def linear_intp(lst, wl, set):
+    return griddata(wl,
+                    np.array(lst),
+                    set,
+                    method='linear')
+
+def gen_columns(param, beg, end):
+    return [param + str(i) for i in range(beg, end)]
+
+def gen_calib_cols(param):
+    return ['CalibData_' + param + '(' + str(i) + ')' for i in range(1, 167)]
