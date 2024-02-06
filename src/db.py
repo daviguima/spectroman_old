9470df136885d42f905858c0094114cbe476f10b
lambdart <lambdart@protonmail.com>
2024-02-06 12:26:19 -0300

    Add methods documentation

--- src/db.py
+++ src/db.py
@@ -13,25 +13,44 @@ class Db:
         self.client = None
 
     def connect(self):
+        """
+        Connect with the mongodb and set the attribute client.
+        """
         self.client = pymongo.MongoClient(self.uri)
         pass
 
     def get_db(self):
+        """
+        Return database name.
+        """
         return self.client[self.name]
 
     def get_coll(self, coll):
+        "Return collection name."
         return self.get_db()[coll]
 
     def get_coll_df(self):
+        """
+        Return collection df.
+        """
         return self.get_coll(self.coll_df)
 
     def get_coll_raw(self):
+        """
+        Return collection raw.
+        """
         return self.get_coll(self.coll_raw)
 
     def get_coll_main(self):
+        """
+        Return collection main.
+        """
         return self.get_coll(self.coll_main)
 
     def insert_doc(self, doc, coll):
+        """
+        Insert document into the collection.
+        """
         try:
             self.connect()
             self.get_coll(coll).insert_one(doc)
@@ -43,6 +62,9 @@ class Db:
             pass
 
     def insert_docs(self, docs, coll):
+        """
+        Insert documents into the collection.
+        """
         try:
             self.connect()
             self.get_coll(coll).insert_many(docs)
@@ -54,6 +76,9 @@ class Db:
             pass
 
     def update_doc(self, filter, values, coll):
+        """
+        Update collection document values.
+        """
         try:
             self.connect()
             self.get_coll(coll).update_one(filter, values)
@@ -65,6 +90,9 @@ class Db:
             pass
 
     def fetch_docs(self, filter, projection, coll):
+        """
+        Fetch documents, this method returns a cursor iterable.
+        """
         cursor = None
         try:
             self.connect()
@@ -77,6 +105,9 @@ class Db:
             return cursor
 
     def remove_doc(self, filter, collation, column):
+        """
+        Remove document using the proper filter.
+        """
         try:
             self.connect()
             self.get_coll(column).delete_one(filter, collation)
